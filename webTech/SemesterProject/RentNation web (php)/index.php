143b5d8dda47a6fa9931fc57ecb2c68174c69a0e
zainabzahid711 <zainabzahid711@gmail.com>
2024-06-30 16:44:27 +0500

    orderConfirmed --- data entered in phpMyAdmin

--- webTech/SemesterProject/RentNation web (php)/index.php
+++ webTech/SemesterProject/RentNation web (php)/index.php
@@ -1,25 +1,33 @@
 <?php
-$host = '127.0.0.1';
-$user = 'root';
-$password = 'your_password';
-$dbname = 'golfcards';
-$port = 3307;
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$dbname = "golfcards";
+$port = "3307";
 
 // Create connection
-$conn = new mysqli($host, $user, $password, $dbname, $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+// Fetch data from the users table
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Username: " . $row["username"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
-
-
-
-
-
-
 
 
 
